7a000fbebf6f2fc1daef898dece413a0baa34372
Ley <y_minh.l@kbtg.tech>
2019-11-19 14:01:43 +0700

    Initial version of product generated by JHipster-6.5.0

---
+++ product/src/main/java/com/demo/product/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.demo.product.config.audit;
---
+++ product/src/main/java/com/demo/product/service/ProductKafkaProducer.java
@@ -0,0 +1,45 @@
+package com.demo.product.service;
+
+import com.demo.product.config.KafkaProperties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductKafkaProducer {
+
+    private final Logger log = LoggerFactory.getLogger(ProductKafkaProducer.class);
+
+    private static final String TOPIC = "topic_product";
+
+    private KafkaProperties kafkaProperties;
+
+    private KafkaProducer<String, String> kafkaProducer;
+
+    public ProductKafkaProducer(KafkaProperties kafkaProperties) {
+        this.kafkaProperties = kafkaProperties;
+    }
+
+    public void init() {
+        log.info("Kafka producer initializing...");
+        kafkaProducer = new KafkaProducer<>(kafkaProperties.getProducerProps());
+        Runtime.getRuntime().addShutdownHook(new Thread(this::shutdown));
+        log.info("Kafka producer initialized");
+    }
+
+    public void send(String message) {
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC, message);
+        try {
+            kafkaProducer.send(record);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    public void shutdown() {
+        log.info("Shutdown Kafka producer");
+        kafkaProducer.close();
+    }
+}
